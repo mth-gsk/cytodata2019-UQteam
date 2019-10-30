2e7f7ee48fb90b17aee438e25346d65bd06ef705
berker <yannick.berker@dkfz-heidelberg.de>
2019-10-30 10:10:28 +0100

    Add data normalization to testDataGenerator

--- dataGenerator/testDataGenerator.py
+++ dataGenerator/testDataGenerator.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import keras
 import numpy as np
+
 from .data_augmentation import random_crop, random_rotation
 
 
@@ -15,6 +16,7 @@ class TestDataGenerator(keras.utils.Sequence):
         n_channels=2,
         patch_per_img=1,
         cropOffset=0,
+        normalize_data=0,
     ):
 
         # storing imported parameters
@@ -23,6 +25,7 @@ class TestDataGenerator(keras.utils.Sequence):
         self.n_channels = n_channels
         self.patch_per_img = patch_per_img
         self.cropOffset = cropOffset
+        self.normalize_data = normalize_data
 
         self.allData = [os.path.join(locationImages, filename)
                         for filename in os.listdir(locationImages)]
@@ -53,7 +56,13 @@ class TestDataGenerator(keras.utils.Sequence):
 
         img = random_crop(img, self.inputDim, self.cropOffset)
 
-        X[0] = img / 255
+        if self.normalize_data == 1:
+            X[0] = img / np.max(img)
+        elif self.normalize_data == 2:
+            # https://github.com/pytorch/examples/blob/97304e232807082c2e7b54c597615dc0ad8f6173/imagenet/main.py#L197-L198
+            X[0] = (img / 255 - [0.485, 0.456, 0.406]) - [0.229, 0.224, 0.225]
+        else:
+            X[0] = img / 255
         return X
 
     def __get_batch(self, index):
